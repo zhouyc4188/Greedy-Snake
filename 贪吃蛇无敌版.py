5db35231a2d110e61401d6fa91dd287763801e65
zhouyc4188 <2841416472@qq.com>
2020-09-20 10:17:59 +0800

    sneek dead version

--- 贪吃蛇无敌版.py
+++ 贪吃蛇无敌版.py
@@ -36,9 +36,6 @@ s.onkeypress(go_down,"S")
 s.onkeypress(go_left,"D")
 s.onkeypress(go_right,"A")
 s.listen()
-h_x=0
-h_y=0
-is_tauch=False
 while True:
     if len(tails)>0:
         h_x=head.xcor()
@@ -58,44 +55,6 @@ while True:
         tail.color("green")
         tail.penup()
         tails.append(tail)     #tail存入tails数组
-    if len(tails) > 2:    
-        for i in range(2,len(tails)-2):
-            if head.distance(tails[i])<=10:
-                is_tauch = True
-                print("game over!\n")
-                break;
-    if is_tauch == True:
-        break;
-        
     time.sleep(0.1)
     s.update()
-    
 s.mainloop()
-
-
-    
-       
-
-import pygame,sys
-from pygame.locals import *
-pygame.init() #初始化pygame模块
-DISPLAYSURF = pygame.display.set_mode((400,300)) #长400，宽300
-pygame.display.set_caption('GAME OVER')
-BLACK = (0,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,128)
- 
-fontObj = pygame.font.Font('freesansbold.ttf',32)
-textSurfaceObj = fontObj.render('GAME OVER',True,GREEN,BLUE)
-textRectObj = textSurfaceObj.get_rect()
-textRectObj.center=(200,150)
- 
-while True:
-	DISPLAYSURF.fill(BLACK)
-	DISPLAYSURF.blit(textSurfaceObj,textRectObj)
-	for event in pygame.event.get():
-		if event.type==QUIT:
-			pygame.quit()
-			sys.exit()
-	pygame.display.update()
-
